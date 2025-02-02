1f63f9d117fda2cc94ed72568298aa255e063342
genn125 <genn125@gmail.com>
2025-02-02 19:44:49 +0300

    Яндекс практикум

--- 1.py
+++ 1.py
@@ -1,33 +1,50 @@
-#Пользователь вводит нужный ему год, например - 2030
-year = 2025
+year = 2030
 
-months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь',
+          'Декабрь']
 
-# Количество дней по месяцам:
-days_in_month_0 = 31  # Январь.
-days_in_month_1 = 29 if year % 4 == 0 else 28
-days_in_month_2 = 31
-days_in_month_3 = 30
-days_in_month_4 = 31
-days_in_month_5 = 30
-days_in_month_6 = 31
-days_in_month_7 = 31
-days_in_month_8 = 30
-days_in_month_9 = 31
-days_in_month_10 = 30
-days_in_month_11 = 31  # Декабрь.
+month_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 
-# Исправьте функцию print_header() так, чтобы она принимала три параметра:
-# год, название месяца и количество дней в месяце.
-# В теле функции замените переменные months[1], year и days_in_month_1
-# на названия соответствующих параметров.
-def print_header(year_value,month_name,duration ):
-    print(month_name,year_value)
+def is_leap_year(year):
+    if year % 4 != 0:
+        is_leap = False
+    else:
+        is_leap = True
+
+    if year % 100 == 0:
+        is_leap = False
+    if year % 400 == 0:
+        is_leap = True
+    return is_leap
+
+
+def get_duration(year_value, month_index):
+    if month_index in [3, 5, 8, 10]:
+        duration = 30
+    elif month_index == 1:
+        duration = 29 if is_leap_year(year_value) else 28
+    else:
+        duration = 31
+
+    return duration
+
+
+def print_header(year_value, month_index):
+    print(year_value,months[month_index])
+    duration = get_duration(year_value, month_index)
     print('Количество дней:', duration)
 
+for month in month_numbers:
+    print_header(year, month)
 
-# Ниже - два вызова функции print_header(). 
-# Не изменяйте этот код: он поможет вам проверить работу.
-print_header(year, months[0], days_in_month_0)
-print_header(year, months[1], days_in_month_1)
+
+
+
+# Удалите эти вызовы функции и вместо них напишите цикл,
+# который обработает список month_numbers
+# и на каждой итерации будет вызывать функцию print_header(),
+# поочерёдно подставляя во второй аргумент значения из списка: 0, 1, 2...
+# print_header(year, 0)
+# print_header(year, 1)
+# print_header(year, 2)
