e0e07ff437a36dc527634a3c6d63b897c68972c7
genn125 <genn125@gmail.com>
2025-02-03 00:00:25 +0300

    Яндекс практикум

--- 1.py
+++ 1.py
@@ -1,9 +1,6 @@
 year = 2030
-
-
 months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-
-
+start_day = 3
 def is_leap_year(year):
     if year % 4 != 0:
         is_leap = False
@@ -16,38 +13,57 @@ def is_leap_year(year):
         is_leap = True
     return is_leap
 
-
-def get_duration(year_value, month_index):
+def get_duration(year, month_index):
     if month_index in [3, 5, 8, 10]:
         duration = 30
     elif month_index == 1:
-        duration = 29 if is_leap_year(year_value) else 28
+        duration = 29 if is_leap_year(year) else 28
     else:
         duration = 31
-
     return duration
 
-
-def print_days():
-    for day in range(1,32):    # Здесь вместо days подставьте диапазон.
+def print_days(days_in_month, start_day):
+    spases = ('   ' * start_day)
+    print(spases, end='')
+    for day in range(1, days_in_month + 1):    # Здесь вместо days подставьте диапазон.
         if day < 10:
             print(day, end='  ')
         else:
             print(day, end=' ')
-        if day % 7 == 0:
+        if (day + start_day)  % 7 == 0:
             print()
 
+def print_header(year, month_index):   # Функция для вывода месяца и года
+    print(months[month_index], year)  # вывод месяца и года
+    print('Пн Вт Ср Чт Пт Сб Вс')
 
-def print_header(year_value, month_index):
-    print(months[month_index], year_value)
-    duration = get_duration(year_value, month_index)
-    print('Количество дней:', duration)
+# Функция, вычисляющая день недели, который приходится на 1 января:(взята откуда то)
+def get_starting_day(year):
+    d = 1
+    m = 13
+    y = year - 1
+    h = (d + (13 * (m + 1)) // 5 + y + (y // 4) - (y // 100) + (y // 400)) % 7
+    return (h + 5) % 7
 
+# Функция, вычисляющая день недели,
+# на который выпадет первое число следующего месяца:
+def adjust_start_day(start_day, days_in_month):
+    result = (start_day + days_in_month) % 7
+    return result
 
-for month_number in range(12):    # Здесь вместо month_numbers подставьте диапазон.
-    print_header(year, month_number)
-    print_days()
+# Вычисляем день недели, на который приходится 1 января года year:
+start_day = get_starting_day(year)
+
+# В цикле вызываем необходимые функции:
+for month_number in range(12):
+    print_header(year, month_number)   # вызов функции для вывода месяца и года
+    duration = get_duration(year, month_number)
+    print_days(duration, start_day)                           # Вызываем функцию и передаём в неё значение duration.
+
+    # Переопределяем переменную start_day:
+    start_day = adjust_start_day(start_day, duration)
+
     print()
-# print('Пн Вт Ср Чт Пт Сб Вс')
+
 
 
