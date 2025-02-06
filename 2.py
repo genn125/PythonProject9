bf22c54ddd352575a7586829352ee2d1bd3df3d6
genn125 <genn125@gmail.com>
2025-02-06 23:26:27 +0300

    Stashchuk Bogdan

--- 2.py
+++ 2.py
@@ -40,21 +40,44 @@
 # print(c)
 ###########################################################################
 
-a={'q':1,'b':2,'c':[]}
-a1=a.copy()             # Копирование через .copy() сохраняет ссылку на "с": [] и изменяет её в обоих объектах
-a1['c'].append(3)
-a1['c'].append(5)       #  {'q': 1, 'b': 2, 'c': [3, 5, 123]}
-a['c'].append(123)      #  {'q': 1, 'b': 2, 'c': [3, 5, 123]}
-print('1 -',a)
-print('2 -',a1)
+# a={'q':1,'b':2,'c':[]}
+# a1=a.copy()             # Копирование через .copy() сохраняет ссылку на "с": [] и изменяет её в обоих объектах
+# a1['c'].append(3)
+# a1['c'].append(5)       #  {'q': 1, 'b': 2, 'c': [3, 5, 123]}
+# a['c'].append(123)      #  {'q': 1, 'b': 2, 'c': [3, 5, 123]}
+# print('1 -',a)
+# print('2 -',a1)
+#
+#
+# from copy import deepcopy
+# a={'q':1,'b':2,'c':[]}
+# a1=deepcopy(a)         # Копирование через deepcopy(a) полностью копирует всё и изменяется всё НЕЗАВИСИМО
+# a1['c'].append(3)
+# a1['c'].append(5)      #  {'q': 1, 'b': 2, 'c': [123]}
+# a['c'].append(123)     #  {'q': 1, 'b': 2, 'c': [3, 5]}
+# print('3 -',a)
+# print('4 -',a1)
+# ###############################################################################
+#
+#
+# def  merge_lists_to_dict(list_one, list_two):
+#
+#     return (list_one+list_two)
+#
+#
+# print(merge_lists_to_dict(list_two=59,list_one=35, ))
+# print(merge_lists_to_dict(50,list_two=35 ))
+# print(merge_lists_to_dict(['a',True,100],['abc'],))
 
 
-from copy import deepcopy
-a={'q':1,'b':2,'c':[]}
-a1=deepcopy(a)         # Копирование через deepcopy(a) полностью копирует всё и изменяется всё НЕЗАВИСИМО
-a1['c'].append(3)
-a1['c'].append(5)      #  {'q': 1, 'b': 2, 'c': [123]}
-a['c'].append(123)     #  {'q': 1, 'b': 2, 'c': [3, 5]}
-print('3 -',a)
-print('4 -',a1)
-###############################################################################
+def update_car_info(**car):
+
+    car["is_available"] = True
+    return car
+
+print(update_car_info(brand='BMW', price=100000))
+# {'brand': 'BMW', 'price': 100000, 'is_available': True}
+# # TypeError: update_car_info() takes 0 positional arguments but 2 were given
+# print(update_car_info('BMW', 100000))
+
+
