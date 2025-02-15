2569b958999b556514ab3738817f6605fdd9aaeb
genn125 <genn125@gmail.com>
2025-02-15 14:50:03 +0300

    Delete Errors.py

--- Errors.py
+++
@@ -1,9 +0,0 @@
-def image_info (kwargs):
-    if ('image_id' not  in kwargs) or ('image_title' not  in kwargs):
-        raise TypeError('Ошибка')
-    return f'Image "{kwargs['image_title']}" has id {kwargs['image_id']}'
-try:
-    print(image_info ({'image_title': 'my cat', 'image_id': 123}))
-    print(image_info ({'image_title': 'my cat'}))
-except TypeError as e:
-    print(e)
